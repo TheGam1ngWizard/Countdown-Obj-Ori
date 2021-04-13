0bcdf59b12e4b8c9a901f5fd29d1e635d598ffaf
RyleeTheGirl <42972486+RyleeTheGirl@users.noreply.github.com>
2021-04-12 20:41:51 -0400

    Create Time.java

---
+++ Time.java
@@ -0,0 +1,32 @@
+package countdown;
+
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Time{
+    private static int interval;
+    private static Timer timer;
+
+    public void runTimer(int setInterval){
+        Scanner sc = new Scanner(System.in);
+        int delay = 5000;
+        int period = 1000;
+        timer = new Timer();
+        interval = setInterval;
+        System.out.print(interval + " ");
+        timer.scheduleAtFixedRate(new TimerTask() {
+
+            public void run() {
+                System.out.print(setInterval() + " ");
+
+            }
+        }, delay, period);
+    }
+
+    private static final int setInterval() {
+        if (interval == 1)
+            timer.cancel();
+        return --interval;
+    }
+}
